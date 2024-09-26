01be98c02efc16a2b85e24eca7bd8b4af8fc7bc6
NoumaanAhamed <noumaanahamed2@gmail.com>
2024-09-26 20:14:54 +0530

    init

---
+++ src/components/spline-model.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Model() {
+    return (
+        <main>
+            <Spline
+                scene="https://prod.spline.design/53hfF4vuHode9LQS/scene.splinecode"
+            />
+        </main>
+    );
+}
